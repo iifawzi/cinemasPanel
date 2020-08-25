3de65ce0d3562c624b9dbe2122365b3e573d97ac
Fawzi E. Abdulfattah <iifawzie@gmail.com>
2020-08-25 21:12:12 +0200

    add styles

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,20 @@
 <template>
-    <div class="test">
-        hala
+    <div class="authPage">
+        Control Panel
     </div>
 </template>
+
+
+<style lang="scss" scoped>
+.authPage {
+    background-color: $white-1;
+    height: 100%;
+    min-height: 100vh;
+    width: 100%;
+    display: flex;
+    flex-flow: row;
+    justify-content: center;
+    align-items: center;
+    font-size: 5rem;
+}
+</style>
