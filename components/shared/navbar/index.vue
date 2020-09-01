b7968eb1ff0b2d9e59ddff4d9054c55d7dabdeb8
Fawzi E. Abdulfattah <iifawzie@gmail.com>
2020-09-01 22:37:55 +0200

    add logout icon

--- components/shared/navbar/index.vue
+++ components/shared/navbar/index.vue
@@ -5,12 +5,12 @@
       <div class="right">
         <div class="languages">
             <img   v-if="language == 'en'"
-            @click="changeLang('ar')" src="~/assets/images/arabic.png" width="20px" />
+            @click="changeLang('ar')" src="~/assets/images/arabic.png" width="18px" />
             <img v-if="language == 'ar'"
-            @click="changeLang('en')" src="~/assets/images/english.png" width="20px" />
+            @click="changeLang('en')" src="~/assets/images/english.png" width="18px" />
         </div>
         <div class="logout">
-            <i class="fas fa-door-open icon"></i>
+           <i :class="['fas fa-sign-out-alt icon', language]"></i>
         </div>
       </div>
     </div>
