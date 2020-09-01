d5d5abfd029fa9a3f2c68f68146d614b5647823c
Fawzi E. Abdulfattah <iifawzie@gmail.com>
2020-09-01 22:33:35 +0200

    add default color, grey

--- components/shared/navbar/index.vue
+++ components/shared/navbar/index.vue
@@ -1,12 +1,39 @@
 <template>
-    <div class="navbar">
-        <div class="navbar__contnet">
-               <i class="fas fa-bars aside__button" @click="$store.dispatch('dashboard/toggleAside')"></i>
-            <div class="right">
-                </div>
+  <div class="navbar">
+    <div class="navbar__contnet">
+      <i class="fas fa-bars aside__button" @click="$store.dispatch('dashboard/toggleAside')"></i>
+      <div class="right">
+        <div class="languages">
+            <img   v-if="language == 'en'"
+            @click="changeLang('ar')" src="~/assets/images/arabic.png" width="20px" />
+            <img v-if="language == 'ar'"
+            @click="changeLang('en')" src="~/assets/images/english.png" width="20px" />
         </div>
+        <div class="logout">
+            <i class="fas fa-door-open icon"></i>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 
+<script>
+import Cookie from "js-cookie";
+import loginForm from "~/components/forms/loginForm";
+export default {
+  methods: {
+    changeLang(language) {
+      Cookie.set("lang",language);
+      this.$store.dispatch("changeLang", language);
+      this.$i18n.locale = language;
+    },
+  },
+  computed: {
+    language() {
+      return this.$store.getters.getLocale;
+    },
+  },
+};
+</script>
 
 <style src='./style.scss' lang='scss' scoped/>
