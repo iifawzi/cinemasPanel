5f0fa8c3bd04631650bfbf1f7eb863c4717a43c3
Fawzi E. Abdulfattah <iifawzie@gmail.com>
2020-09-02 03:57:55 +0200

    add sidenav

--- components/shared/aside/index.vue
+++ components/shared/aside/index.vue
@@ -14,12 +14,99 @@
       />
       <img v-else alt="website logo" src="~/assets/images/letter.png" class="logo" width="20px" />
     </div>
-    <div class="bottom"></div>
+    <ul class="bottom">
+
+      <li class="section">
+        <nuxt-link class="link" to="/dashboard">
+          <span class="icon">
+            <i class="fas fa-home"></i>
+          </span>
+          <span class="title">Dashboard</span>
+        </nuxt-link>
+      </li>
+
+      <li class="main-section">
+        <span class="title">Preferences</span>
+      </li>
+
+      <li class="section">
+        <nuxt-link class="link" to="/dashboardddd">
+          <span class="icon">
+            <i class="fas fa-film"></i>
+          </span>
+          <span class="title">Halls</span>
+        </nuxt-link>
+      </li>
+
+      <li class="section">
+        <nuxt-link class="link" to="/dashboardddd">
+          <span class="icon">
+            <i class="fas fa-calendar-check"></i>
+          </span>
+          <span class="title">Shows</span>
+        </nuxt-link>
+      </li>
+      
+      <li class="section">
+        <nuxt-link class="link" to="/dashboardddd">
+          <span class="icon">
+            <i class="fas fa-ticket-alt"></i>
+          </span>
+          <span class="title">Tickets</span>
+        </nuxt-link>
+      </li>
+
+<li class="main-section">
+        <span class="title">Payments</span>
+      </li>
+
+      <li class="section">
+        <nuxt-link class="link" to="/dashboardddd">
+          <span class="icon">
+            <i class="fas fa-money-check-alt"></i>
+          </span>
+          <span class="title">Diposits</span>
+        </nuxt-link>
+      </li>
+
+        <li class="section">
+        <nuxt-link class="link" to="/dashboardddd">
+          <span class="icon">
+            <i class="fas fa-file-pdf"></i>
+          </span>
+          <span class="title">Reports</span>
+        </nuxt-link>
+      </li>
+
+      <li class="main-section">
+        <span class="title">Control</span>
+      </li>
+
+      <li class="section">
+        <nuxt-link class="link" to="/dashboardddd">
+          <span class="icon">
+            <i class="fas fa-user-astronaut"></i>
+          </span>
+          <span class="title">Admins & Modirators</span>
+        </nuxt-link>
+      </li>
+
+        <li class="section">
+        <nuxt-link class="link" to="/dashboardddd">
+          <span class="icon">
+            <i class="fas fa-question-circle"></i>
+          </span>
+          <span class="title">Support</span>
+        </nuxt-link>
+      </li>
+      
+
+    </ul>
   </aside>
 </template>
 
 <script>
-import badge from "~/components/shared/badge"
+import badge from "~/components/shared/badge";
 export default {
   computed: {
     getAsideStatus() {
@@ -33,8 +120,8 @@ export default {
     },
   },
   components: {
-      badge,
-  }
+    badge,
+  },
 };
 </script>
 
