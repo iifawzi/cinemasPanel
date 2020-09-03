372a8a60cdb08232508f26086502f03e2f9e1b40
Fawzi E. Abdulfattah <iifawzie@gmail.com>
2020-09-03 19:42:43 +0200

    update colors

--- components/shared/pageInfo/index.vue
+++ components/shared/pageInfo/index.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="pageInfo">
         <div class="content">
-            <span class="pagetitle">Add Hall <span class="line$grey-1">|</span></span> <span class="description">You can add a halls to your cinema</span>
+            <span class="pagetitle">Add Hall <span class="line">|</span></span> <span class="description">You can add a halls to your cinema</span>
         </div>
     </div>
 </template>
