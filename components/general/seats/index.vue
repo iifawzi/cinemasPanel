9a32d3b7cad39781457277017e9113bb4c376ec0
Fawzi E. Abdulfattah <iifawzie@gmail.com>
2020-09-04 22:11:08 +0200

    init seats|

--- components/general/seats/index.vue
+++ components/general/seats/index.vue
@@ -1,13 +1,21 @@
 <template>
-    <div class="seats">
-        d
-    </div>
+  <div class="seats">
+    
+  </div>
 </template>
 
 <script>
+import seat from "~/components/shared/seat/";
 export default {
-   
-}
+  components: {
+    seat,
+  },
+  computed: {
+      language(){
+          return this.$store.getters.getLocale;
+      }
+  }
+};
 </script>
 
 <style src='./style.scss' lang='scss' scoped />
