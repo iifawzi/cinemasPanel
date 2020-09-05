74e71a8c0e2a5ccd6980eda73ef9ec07c9cf137d
Fawzi E. Abdulfattah <iifawzie@gmail.com>
2020-09-05 06:13:56 +0200

    using locales

--- components/general/seats/index.vue
+++ components/general/seats/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="seats" dir="ltr">
     <div class="content">
-      <div class="screen">Screen</div>
+      <div class="screen">{{$t("general.screen")}}</div>
       <div class="data">
         <div class="rowsNumbers">
           <div class="rowNumber">-</div>
