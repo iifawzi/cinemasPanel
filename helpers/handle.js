717b6c85bc000689de2a400260080f9c4aeb34df
Fawzi E. Abdulfattah <iifawzie@gmail.com>
2020-09-14 20:16:39 +0200

    add api's calls

---
+++ helpers/handle.js
@@ -0,0 +1,27 @@
+// * when used with await
+// *
+// * - Example response [ data, undefined ]
+// * - Example response [ undefined, Error ]
+// *
+// *
+// * When used with Promise.all([req1, req2, req3])
+// * - Example response [ [data1, data2, data3], undefined ]
+// * - Example response [ undefined, Error ]
+// *
+// *
+// * When used with Promise.race([req1, req2, req3])
+// * - Example response [ data, undefined ]
+// * - Example response [ undefined, Error ]
+// *
+// * @param {Promise} promise
+// * @returns {Promise} [ data, undefined ]
+// * @returns {Promise} [ undefined, Error ]
+// *
+
+const handle = (promise) => {
+ return promise
+   .then(data => ([data, undefined]))
+   .catch(error => Promise.resolve([undefined, error]));
+}
+
+module.exports = handle;
