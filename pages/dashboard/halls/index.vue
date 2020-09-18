fbe3a8f8934fd9922a5e19e52561dace1c91b90e
Fawzi E. Abdulfattah <iifawzie@gmail.com>
2020-09-18 06:27:59 +0200

    init halls' page

--- pages/dashboard/halls/index.vue
+++ pages/dashboard/halls/index.vue
@@ -1,11 +1,14 @@
 <template>
-    <div class="halls">
-        
+    <div class="addhall">
+        <pageInfo :title="$t('pages.halls.title')" :desc="$t('pages.halls.desc')"/>
+        <div class="content">
+        </div>
     </div>
 </template>
 
 
 <script>
+import pageInfo from "~/components/shared/pageInfo"
 export default {
     head() {
     return {
@@ -13,5 +16,20 @@ export default {
     };
   },
     layout: 'dashboard',
+    components: {
+        pageInfo,
+    }
 }
 </script>
+
+
+<style lang="scss" scoped>
+.content {
+  background-color: $white;
+  width: 95%;
+  margin: 25px auto;
+  min-height: 500px;
+  border-radius: 10px;
+  box-shadow: 0px 0px 20px 0px $grey-1;
+}
+</style>
