563c5093cc1f46623ec9eeffe208ce1e68b41e68
Fawzi E. Abdulfattah <iifawzie@gmail.com>
2020-09-18 06:15:56 +0200

    update aside-link

---
+++ pages/dashboard/halls/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="halls">
+        
+    </div>
+</template>
+
+
+<script>
+export default {
+    head() {
+    return {
+      title: "Halls",
+    };
+  },
+    layout: 'dashboard',
+}
+</script>
