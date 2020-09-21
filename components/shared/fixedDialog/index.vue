bb89d2af41bbe32b42288e16f733d90bb530d961
Fawzi E. Abdulfattah <iifawzie@gmail.com>
2020-09-21 12:17:24 +0200

    add fixedDialog

---
+++ components/shared/fixedDialog/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="fixedDialog" @click="emitClose($event)">
+    <div class="content">
+        <span class="closeIcon"><i class="fas fa-times" @click="emitClose($event)"></i></span>
+    <div class="slotContent">
+          <slot />
+    </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+methods: {
+    emitClose(e){
+        if (e.target === e.currentTarget){
+        this.$emit("askToClose");
+        }
+    } 
+}
+}
+</script>
+
+<style src="./style.scss" lang="scss" scoped />
