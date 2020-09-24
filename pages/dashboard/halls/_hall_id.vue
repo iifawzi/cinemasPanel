01be1e6424df8b16ce1590c14ff5d020016297eb
Fawzi E. Abdulfattah <iifawzie@gmail.com>
2020-09-24 08:41:56 +0200

    checking if hall is not found

--- pages/dashboard/halls/_hall_id.vue
+++ pages/dashboard/halls/_hall_id.vue
@@ -1,32 +1,54 @@
 <template>
-    <div class="edithall">
-        <pageInfo :title="$t('pages.edithall.title')" :desc="$t('pages.edithall.desc')"/>
-        <div class="content">
-          <edithallForm/>
-        </div>
+  <div class="edithall">
+    <pageInfo :title="$t('pages.edithall.title')" :desc="$t('pages.edithall.desc')" />
+    <div class="content">
+      <edithallForm v-if="!loading && !error" />
+      <span v-if="error && !loading" class="not-found">لم يتم العثور على القاعه عذرًا</span>
+      <loading type="circles" v-if="loading" />
     </div>
+  </div>
 </template>
 
 
 <script>
-import pageInfo from "~/components/shared/pageInfo"
-import edithallForm from "~/components/forms/editHall/main/"
+import handle from "~/helpers/handle";
+import pageInfo from "~/components/shared/pageInfo";
+import edithallForm from "~/components/forms/editHall/main/";
+import loading from "~/components/shared/loading";
 
 export default {
-    mounted(){
-        console.log(this.$route.params);
-    },
-    head() {
+  async mounted() {
+    this.loading = true;
+    const hall_id = this.$route.params.hall_id;
+    const [hall, hall_error] = await handle(
+      this.$api.post("halls/getHall", { hall_id })
+    );
+    this.loading = false;
+    if (hall) {
+      this.hallData = hall;
+    } else {
+      this.error = true;
+    }
+  },
+  head() {
     return {
       title: "Edit Hall",
     };
   },
-    layout: 'dashboard',
-    components: {
-        pageInfo,
-        edithallForm
-    }
-}
+  layout: "dashboard",
+  components: {
+    pageInfo,
+    edithallForm,
+    loading,
+  },
+  data() {
+    return {
+      loading: false,
+      error: false,
+      hallData: ""
+    };
+  },
+};
 </script>
 
 
@@ -35,8 +57,14 @@ export default {
   background-color: $white;
   width: 95%;
   margin: 25px auto;
-  min-height: 500px;
+  padding: 25px 0px;
+  height: auto;
   border-radius: 10px;
   box-shadow: 0px 0px 20px 0px $grey-1;
+  .not-found{
+  display: block;
+  text-align: center;
+  color: $red;
+  }
 }
 </style>
