5d0b98537ac34c86abe1e5e22da020b5cbb98d7f
Fawzi E. Abdulfattah <iifawzie@gmail.com>
2020-10-29 00:52:21 +0200

    update to min/

--- middleware/isAuth.js
+++ middleware/isAuth.js
@@ -5,7 +5,7 @@ export default function ({redirect,store}){
         const token = Cookie.get("authorization");
         const decoded = jwt_decode(token);
         const exp = decoded.exp; // in seconds
-        const current_time = new Date().getTime() / 1000; // in seconds
+        const current_time = new Date().getTime() / 60000; // in minutes
         const fiveMinAfter = current_time + 60*5; // five minutes after the current time (this will be used to ask for new token before the old one ends)
         if (fiveMinAfter > exp){ // token is expired
             redirect("/");
